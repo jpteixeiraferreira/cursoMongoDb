d0d7c575a11e2f552d94afa86ee3c4607be1d2e8
João Pedro <jpteixeiraferreira@gmail.com>
2025-10-16 17:57:17 -0300

    Implementação do sistema de cadastro de usuários

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
-const helmet = require('helmet');
 const csrf = require('csurf');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 
-const routes = require('./routes');
 const {middleware, checkCsrfError, csrfMiddleware} = require('./src/middlewares/middleware');
+const routes = require('./routes');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(cookieParser());
 
 //conexão com o banco
 mongoose.connect(process.env.CONNECTIONSTRING)
@@ -24,11 +28,7 @@ mongoose.connect(process.env.CONNECTIONSTRING)
         console.error("Conexão com o banco de dados falhou " + err);
     });
 
-app.use(helmet());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.use(express.json());
-app.use(cookieParser());
+
 
 const sessionOptions = session({
     secret: process.env.SESSIONSECRET,
@@ -48,6 +48,7 @@ app.use(csrf({cookie: true}));
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(middleware);
+
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,12 +1,14 @@
 exports.middleware = (req, res, next) => {
-    res.locals.varLocal = 'VarLocal';
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err && 'EBADCSRFTOKEN' === err.code){
+    if(err){
         return res.render('404');
     }
+    next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
